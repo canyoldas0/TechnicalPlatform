7aae450e57726f71fe242440427161488edb2b46
Can Yoldas <canyoldas1997@gmail.com>
2023-01-01 21:00:36 +0100

    updated db key

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('./config.js');
+
 
 // express app
 const app = express();
 app.use(express.json());
 
 mongoose.set("strictQuery", false);
-mongoose.connect(dbURI)
+console.log(config.dbKey);
+mongoose.connect(config.dbKey)
 .then( (result) => {
     console.log('connected')
     app.listen(3000)
 })
+
 .catch( (err) => console.log('error during connecting'));
 
