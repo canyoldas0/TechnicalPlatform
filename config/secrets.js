3357817a5e6e6e30fda9013077ed0b9060f2fbf3
Can Yoldas <canyoldas1997@gmail.com>
2023-05-03 01:17:23 +0200

    wip

--- config/secrets.js
+++ config/secrets.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-    dbKey: process.env.DB_KEY,
-    jwtSecret: process.env.JWT_KEY
-}
+  dbKey: process.env.DB_KEY,
+  jwtSecret: process.env.JWT_KEY,
+};
