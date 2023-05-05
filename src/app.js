792088dbea240998b6ea6513ee481e9c8068407b
Can Yoldas <canyoldas1997@gmail.com>
2023-05-05 23:29:48 +0200

    added graphql

--- src/app.js
+++ src/app.js
@@ -3,28 +3,54 @@ const secrets = require('../config/secrets');
 const router = require('./api/auth/router.js');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // User model
 const User = require('./models/user');
-
 
 // express app
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 app.use('/api/auth', router);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-mongoose.set("strictQuery", false);
-console.log(secrets.dbKey);
-mongoose.connect(secrets.dbKey)
-.then( (result) => {
-    console.log('connected')
-    
-app.listen(port, () => console.log(`Server started on port ${port}`));
-})
+mongoose
+  .connect(secrets.dbKey, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error', err);
+  });
 
-.catch( (err) => console.log('error during connecting'));
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log('Server started on port 3000');
+});
